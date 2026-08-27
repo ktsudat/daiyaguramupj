81d8ee6b65ca350891a27d60f14d1bea80cd7888
ktsudat <oitarailway@gmail.com>
2026-08-27 16:18:51 +0900

    お知らせ追加

---
+++ notices.js
@@ -0,0 +1,7 @@
+const ADMIN_NOTICES = [
+  {
+    date: "2026/08/27",
+    text: "おおいた地域交通見える化ボードの方でアンケートを実施中です。可能であればご協力のほどお願いします。\n対象の方は「飛行機への搭乗を目的として大分空港を利用した方」です。",
+    url: "https://ktsudat.github.io/mierukaPJ/survey.html"
+  }
+];
